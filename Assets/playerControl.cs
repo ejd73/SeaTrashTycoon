72d73d1d96ceb544aae229a85000399a20840b4b
ejd73 <ejd73@pitt.edu>
2024-10-02 21:26:04 -0400

    initial test

---
+++ Assets/playerControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+
+    public float movementSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movementSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}

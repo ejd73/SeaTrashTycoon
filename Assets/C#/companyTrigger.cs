9eef74b42f4eb98453a2a68fb9205c620075acc0
Song <songxingyu1118@outlook.com>
2024-12-03 23:48:32 -0500

    updated company setting

---
+++ Assets/C#/companyTrigger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class companyTrigger : MonoBehaviour
+{
+
+
+     public GameObject uiObject;
+     public void Start()
+     {
+          uiObject.SetActive(false);
+     }
+     private void OnTriggerEnter2D()
+     {
+          Debug.Log("We just collided");
+          uiObject.SetActive(true);
+     }
+
+     private void OnTriggerStay2D()
+     {
+          Debug.Log("We still collided");
+     }
+
+     private void OnTriggerExit2D()
+     {
+
+          uiObject.SetActive(false);
+          Debug.Log("We quit collided");
+     }
+}
+

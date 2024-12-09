d1dce40efad90f54cea5accbe268ea84c2d4a09d
ejd73 <ejd73@pitt.edu>
2024-12-09 12:55:53 -0500

    build - changed ui positioning to be readable and in screen in game

--- Assets/C#/companyTrigger.cs
+++ Assets/C#/companyTrigger.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class companyTrigger : MonoBehaviour
 {
+     public GameObject uiObjectToEnable; // UI to show
+     public GameObject uiObjectToDisable; // UI to hide
 
-
-     public GameObject uiObject;
      public void Start()
      {
-          uiObject.SetActive(false);
+          uiObjectToEnable.SetActive(false);
+          uiObjectToDisable.SetActive(true);
      }
-     private void OnTriggerEnter2D()
+
+     private void OnTriggerEnter2D(Collider2D collision)
      {
           Debug.Log("We just collided");
           InventoryManager.Instance.DisplayImpact();
-          uiObject.SetActive(true);
+
+          // Show one UI element and hide the other
+          uiObjectToEnable.SetActive(true);
+          uiObjectToDisable.SetActive(false);
      }
 
-     private void OnTriggerStay2D()
+     private void OnTriggerStay2D(Collider2D collision)
      {
           Debug.Log("We still collided");
      }
 
-     private void OnTriggerExit2D()
+     private void OnTriggerExit2D(Collider2D collision)
      {
-
-          uiObject.SetActive(false);
+          // Revert the UI changes when exiting the trigger
+          uiObjectToEnable.SetActive(false);
+          uiObjectToDisable.SetActive(true);
           Debug.Log("We quit collided");
      }
 }
-
